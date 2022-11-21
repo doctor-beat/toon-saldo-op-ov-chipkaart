416e80b4561ea3e20df1c7c75b7bc4a452d3c32a
doctor-beat <doctor-beat@users.noreply.github.com>
2022-11-21 12:09:07 +0100

    Create user-script.js

---
+++ user-script.js
@@ -0,0 +1,60 @@
+if ('undefined' == typeof window.jQuery) {
+	alert("ERROR: Geen jquery gevonden.");
+} else {
+	$(function(){
+		console.log(window.location.pathname);
+		//let regex = ;
+		
+		//only run on home page:
+		if (/^\/(my|mijn)\-ov\-chip\.htm/.test(window.location.pathname)) {
+			
+		    var firstContainer = $('div#content div.translink-mf-container').first();
+		
+		    var reisHistorieButton = $(firstContainer).find('div.tls-button').first();
+		    reisHistorieButton.after('<br/><div class="flex-cell-content tls-button  xc-edit" style="background:orange; box-shadow:none">' +
+		            '<a href="/mijn-ov-chip/kaartinformatie/check-je-saldo.htm" class="" title="Check je saldo">Saldo checken</a>' +
+		            '</div>');
+		            
+		            
+		    $.get( "/mijn-ov-chip/kaartinformatie/check-je-saldo.htm", function( data ) {
+				//console.log( "Data Loaded: " + data );
+				
+				var saldoInfo = $("#ol_cardselector", data);
+				console.log(saldoInfo);
+				
+				saldoInfo.css("visibility", "visible");
+				$("li:first", saldoInfo).addClass("active");		//?????
+				$( saldoInfo ).prependTo("div#content > div");
+				
+				$("ol#ol_cardselector li.list-group-item").each(function(index, element ) {
+					//parse the card info:
+					var hashed = $("div.cs-card-title-content span.cs-card-number").data("hashed");
+					console.log("hashed: ", hashed);
+					if (!hashed) {
+						console.warn("No hashed value found for entry " + index);
+					} else {
+						$("dl.cs-card-details", element).show();
+						$.post( 
+							"/web/medium_information", 
+							{hashedMediumId: hashed, languagecode: "nl-NL"},
+							function( data ) {
+								console.log(data);
+								//console.log($("dd.cs-card-details-profile", element).text());
+								//console.log(data.mediumTypeDescription);
+								
+								$("dd.cs-card-details-profile", element).text(data.mediumTypeDescription);
+								$("dd.cs-card-details-valid", element).text(data.expiryDate);
+								$("dd.cs-card-details-status", element).text(data.mediumStatusDescription);
+								$("dd.cs-card-details-saldo", element).text(data.localeEPurseRemainingAmount)
+								$("dd.cs-card-details-saldo", element).parent().css("background-color", "#FFFFA8");
+								$("dd.cs-card-details-saldodate", element).text(data.lastActivityTime);
+								//cs-card-details-status
+							},
+							"json"
+						);
+					}
+				});
+			});
+		}
+	});
+}
