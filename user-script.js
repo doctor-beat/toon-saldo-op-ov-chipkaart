2affa08e40c4bc8aeb9a90c4d430a16e2059eb3e
doctor-beat <doctor-beat@users.noreply.github.com>
2023-07-31 15:27:24 +0200

    Modified for 2023 style ov website

--- user-script.js
+++ user-script.js
@@ -2,59 +2,69 @@ if ('undefined' == typeof window.jQuery) {
 	alert("ERROR: Geen jquery gevonden.");
 } else {
 	$(function(){
-		console.log(window.location.pathname);
+		console.log("OV chip versie 2023: " + window.location.pathname);
 		//let regex = ;
 		
 		//only run on home page:
-		if (/^\/(my|mijn)\-ov\-chip\.htm/.test(window.location.pathname)) {
-			
-		    var firstContainer = $('div#content div.translink-mf-container').first();
-		
-		    var reisHistorieButton = $(firstContainer).find('div.tls-button').first();
-		    reisHistorieButton.after('<br/><div class="flex-cell-content tls-button  xc-edit" style="background:orange; box-shadow:none">' +
-		            '<a href="/mijn-ov-chip/kaartinformatie/check-je-saldo.htm" class="" title="Check je saldo">Saldo checken</a>' +
-		            '</div>');
-		            
-		            
-		    $.get( "/mijn-ov-chip/kaartinformatie/check-je-saldo.htm", function( data ) {
-				//console.log( "Data Loaded: " + data );
-				
-				var saldoInfo = $("#ol_cardselector", data);
-				console.log(saldoInfo);
-				
-				saldoInfo.css("visibility", "visible");
-				//$("li", saldoInfo).addClass("active");		//?????
-				$( saldoInfo ).prependTo("div#content > div");
-				
-				$("ol#ol_cardselector li.list-group-item").each(function(index, element ) {
-					//parse the card info:
-					var hashed = $("span.cs-card-number", element).data("hashed");
-					console.log("hashed: ", hashed);
-					if (!hashed) {
-						console.warn("No hashed value found for entry " + index);
-					} else {
-						$("dl.cs-card-details", element).show();
-						$.post( 
-							"/web/medium_information", 
-							{hashedMediumId: hashed, languagecode: "nl-NL"},
-							function( data ) {
-								console.log(data);
-								//console.log($("dd.cs-card-details-profile", element).text());
-								//console.log(data.mediumTypeDescription);
-								
-								$("dd.cs-card-details-profile", element).text(data.mediumTypeDescription);
-								$("dd.cs-card-details-valid", element).text(data.expiryDate);
-								$("dd.cs-card-details-status", element).text(data.mediumStatusDescription);
-								$("dd.cs-card-details-saldo", element).text(data.localeEPurseRemainingAmount)
-								$("dd.cs-card-details-saldo", element).parent().css("background-color", "#FFFFA8");
-								$("dd.cs-card-details-saldodate", element).text(data.lastActivityTime);
-								//cs-card-details-status
-							},
-							"json"
-						);
-					}
+		if (/\/(my|mijn)\-ov\-chip(\.htm)?/.test(window.location.pathname)) {
+			//first get the bearer token:
+		    $.get( "/api/auth/session", function( data ) {
+				//console.log( "Data Loaded (session): ", data );
+				var idToken = data.id_token;
+				console.log( " - id token: ", idToken);
+
+				//get the card-list:
+				$.ajax({
+					url: "/backend/moc/cards/retrieve",
+					type: 'GET',
+					contentType: 'application/json',
+					headers: {
+						'Authorization': 'Bearer ' + idToken
+					},
+					success: function( data ) {
+						console.log( "Cards data Loaded: ", data );
+
+						//add the card div to html
+						$('<div class="cards"><h3>Kaarten</h3><ol id="cardList"></ol></div>')
+							.prependTo("main:first section:first");
+						$(data.cards).each(function(index, element ) {
+							var hashed = element.hashedMediumId;
+							//console.log("hashed: ", hashed);
+							if (!hashed) {
+								console.warn("No hashed value found for entry " + index);
+							} else {
+								//get the card details:
+								$.ajax({ 
+									url: "/backend/moc/cards/retrievedetails", 
+									data: {hashedMediumId: hashed, languagecode: "nl-NL"},
+									type: 'GET',
+									contentType: 'application/json',
+									headers: {
+										'Authorization': 'Bearer ' + idToken
+									},
+									success: function( data ) {
+										console.log("card details: ", data);
+										console.log(" - saldo: ", data.cardStatus.amount);
+
+										var lastActivityTime = data.cardStatus.lastActivityTime.replace('T', ' ');
+										var cardId = data.mediumId.replace(/.{4}/g, '$& ');
+										var html = `<li><div>\
+											<span class="alias">${data.alias}</span>\
+											<span class="id">${cardId}</span>\
+											<span class="saldo">Saldo: &euro; ${data.cardStatus.amount} </span>\
+											<span class="lasttime">(${lastActivityTime})</span>\
+											<span class="valid">Geldig tot ${data.cardStatus.expiryDate}</span>\
+											<span class="status">${data.cardStatus.status}</span>\
+											</div></li>`;
+										$(html).appendTo("#cardList");
+									}
+								});
+							}
+						});
+					},
 				});
 			});
+
 		}
 	});
 }
